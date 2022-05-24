5a4b537dec478593593f24a50e7e9ed7b7cf8c3c
shengshengyang <97731209+shengshengyang@users.noreply.github.com>
2022-05-24 20:19:26 +0800

    更改上傳圖片路徑

--- src/main/java/controller/ProductImageServlet.java
+++ src/main/java/controller/ProductImageServlet.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
+import javax.naming.InitialContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class ProductImageServlet
@@ -22,34 +25,32 @@ import javax.servlet.http.Part;
 @WebServlet("/")
 public class ProductImageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProductImageServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	DataSource ds =null;
+	InitialContext ctx = null;
+	Connection con = null;
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 		System.out.println("In do post method of Add Image servlet.");
 		Part file=request.getPart("image");
 		
 		String imageFileName=file.getSubmittedFileName();  // get selected image file name
 		System.out.println("Selected Image File Name : "+imageFileName);
 		
-		String uploadPath="/Users/dean/Desktop/JavaEE/servletWS/fileupload/src/main/webapp/WEB-INF/lib/"+imageFileName;  // upload path where we have to upload our actual image
+		String uploadPath="/vegetarian/src/main/webapp/images/GoodsPhoto/"+imageFileName;  // upload path where we have to upload our actual image
 		System.out.println("Upload Path : "+uploadPath);
 		
 		// Uploading our selected image into the images folder
@@ -73,18 +74,20 @@ public class ProductImageServlet extends HttpServlet {
 		}
 		//**********************
 		
-		//getting database connection (jdbc code)
-		Connection connection=null;
+
+		
 		try 
 		{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/veganDB","test","test");
+			ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/veganDB");
+			
+			con=ds.getConnection();
 			PreparedStatement stmt;
 			String name = request.getParameter("name");
 			String category = request.getParameter("category");
 			double price = Double.parseDouble(request.getParameter("price"));
 			String query="insert into products(name,category,price,image) values(?,?,?,?)";
-			stmt=connection.prepareStatement(query);
+			stmt=con.prepareStatement(query);
 			stmt.setString(1,name);
 			stmt.setString(2,category);
 			stmt.setDouble(3,price);
@@ -94,7 +97,8 @@ public class ProductImageServlet extends HttpServlet {
 			if(row>0)
 			{
 				System.out.println("成功新增商品");
-				
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/backend.jspf");
+				dispatcher.forward(request, response);
 			}
 			
 			else
